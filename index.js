141f19ed5c2be4854bce645ea872bfb46a71accf
angelaWANG000 <wang.shuting89@hotmail.com>
2019-08-21 21:24:08 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const Koa = require('koa')
+const fs = require('fs')
+const path = require('path')
+const views = require('koa-views');
+const static = require('koa-static')
 const app = new Koa()
 
-app.use( async ( ctx ) => {
-  ctx.body = 'hello koa2'
+const Router = require('koa-router')
+const staticPath = './static'
+
+let router = new Router()
+app.use(static(
+  path.join( __dirname,  staticPath)
+))
+app.use(views(__dirname + '/templates',{
+  extension: 'html'
+}))
+router.get('/', async ( ctx )=>{
+  await ctx.render("index.html")
+}).get('/meter', async (ctx) => {
+  await ctx.render('meter.html')
+}).get('/tel', async (ctx) => {
+  await ctx.render('tel.html')
+}).get('/cat', async (ctx) => {
+  await ctx.render("cat.html")
 })
+
+app.use(router.routes())
 
 app.listen(3000)
 console.log('[demo] start-quick is starting at port 3000')
